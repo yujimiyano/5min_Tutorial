1585e3cc048d7af6d9f7152e1769414acfb59908
Yuji Miyano <YujiMiyano@72.inst-3.ufg.ac.at>
2015-09-22 18:25:56 +0200

    First pages commit

--- main-sp.js
+++ main-sp.js
@@ -1,14 +1,38 @@
 window.onload = function(){
 
-      var output = document.getElementById('output');
+    var currentMode = 'portrait';
+    var output = document.getElementById('output');
 
-      window.addEventListener('devicemotion', function(e){
+    var milkcocoa = new MilkCocoa("app_id.mlkcca.com");
+    var ds = milkcocoa.dataStore('gravity');
+
+    window.addEventListener('devicemotion', function(e){
         gravity = e.accelerationIncludingGravity;
 
         output.innerHTML
         = 'x方向: '+gravity.x
         + '<br>y方向: '+gravity.y;
 
-        },true);
+        sendModeFromGravityValue(gravity);
 
+    },true);
+
+    function sendModeFromGravityValue(g){
+
+        // 絶対値を取得
+        var x = Math.sqrt(g.x * g.x);
+        var y = Math.sqrt(g.y * g.y);
+
+        // portrait -> landscape
+        if(currentMode === 'portrait' && x > 8.5 && y < 1.5){
+            currentMode = 'landscape';
+            ds.send({mode: currentMode});
+        }
+
+        // landscape -> portrait
+        if(currentMode === 'landscape' && x < 1.5 && y > 8.5){
+            currentMode = 'portrait';
+            ds.send({mode: currentMode});
+        }
+    }
 };
